f3d0f2f9818cbcd0b01d754486ce6f09817c9a02
Amrinder Singh <57304073+Amrinder1822k@users.noreply.github.com>
2021-02-14 16:08:38 -0500

    Add files via upload

---
+++ Assignment-2/Assignment-2-index.php
@@ -0,0 +1,25 @@
+Date:
+<?php
+echo "Today is :Saturday April 6th, 2019 ";
+?>
+
+Time:
+<?php
+echo "10.25am " ;
+?>
+
+<?php
+echo "
+Your table details :
+Diameter: 10
+Circumference : 10
+Surface area : 10";
+?>
+
+<?php
+echo "
+The length of your triangle hypotenuse :
+Side a : 10
+Side b :10
+Hypotenuse : 10";
+?>
