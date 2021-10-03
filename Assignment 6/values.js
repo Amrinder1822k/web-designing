4f4e049631ad3ecf7b9897b02d66094c8cbaef66
Amrinder Singh <57304073+Amrinder1822k@users.noreply.github.com>
2021-10-02 21:55:26 -0400

    Add files via upload

---
+++ Assignment 6/values.js
@@ -0,0 +1,7 @@
+ var fname = prompt('What is your First name?');
+ var lname = prompt('What is your Last name?');
+ var age = prompt('What is your Age?');
+document.write("I am " + fname + lname + " and I am " + age + " year old and this is Amrinder's 6th assignment. Thanks");
+document.body.appendChild(document.createElement('p'));
+document.querySelector("body p:last-child").id = "jstext";
+document.querySelector("#jstext").innerHTML;
